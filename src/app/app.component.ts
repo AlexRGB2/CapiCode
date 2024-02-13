b9908a52e7ef1b615b505456cfbc8bbe3916ee07
AlexRGB <alexismtz200326@gmail.com>
2024-02-13 13:28:50 -0600

    [#3] Nuevos Componentes para UX.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './pages/header/header.component';
 import { initFlowbite } from 'flowbite';
 import { FooterComponent } from './pages/footer/footer.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { ScrollToTopButtonComponent } from './scroll-to-top-button/scroll-to-top-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
   imports: [
     RouterOutlet,
     HeaderComponent,
     FooterComponent,
     BreadcrumbModule,
     RouterModule,
+    ScrollToTopButtonComponent,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'CapiCode';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/auth/register/register.component';
 import { LoginComponent } from './pages/auth/login/login.component';
+import { PlanesComponent } from './pages/planes/planes.component';
+import { PoliticaPrivacidadComponent } from './pages/politica-privacidad/politica-privacidad.component';
+import { TerminosCondicionesComponent } from './pages/terminos-condiciones/terminos-condiciones.component';
 
 export const routes: Routes = [
   {
@@ -29,6 +32,27 @@ export const routes: Routes = [
     component: RegisterComponent,
     data: {
       breadcrumb: 'Crear Cuenta',
+    },
+  },
+  {
+    path: 'plans',
+    component: PlanesComponent,
+    data: {
+      breadcrumb: 'Planes',
+    },
+  },
+  {
+    path: 'privacy-policy',
+    component: PoliticaPrivacidadComponent,
+    data: {
+      breadcrumb: 'Política de Privacidad',
+    },
+  },
+  {
+    path: 'terms-conditions',
+    component: TerminosCondicionesComponent,
+    data: {
+      breadcrumb: 'Términos y Condiciones',
     },
   },
   {
