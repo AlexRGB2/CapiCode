c47e94f37737111f2582c67a2cb5854408276d9a
AlexRGB2 <alexismtz200326@gmail.com>
2024-02-27 17:42:18 -0600

    [#2] Enlaces footer

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.darkMode();
   }
+
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
